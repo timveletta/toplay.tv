6aa2c8d50c8cc0e70eed1e10a6c2d6ac132f640a
Tim Veletta <tim.veletta@mechanicalrock.io>
2020-02-16 17:26:16 +0800

    feat: using cloudformation to build infrastructure

--- infrastructure/bin/infrastructure.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "@aws-cdk/core";
-import { BaseStack } from "../lib/base-stack";
-
-const app = new cdk.App();
-new BaseStack(app, "BaseStack");
