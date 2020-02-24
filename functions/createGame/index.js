326d8edf7644340abb9a2c75dc614a73f9321e13
Tim Veletta <tim.veletta@mechanicalrock.io>
2020-02-25 00:36:47 +0800

    feat: adding appsync api

--- functions/createGame/index.js
+++ functions/createGame/index.js
@@ -1,13 +1,32 @@
+const AWS = require("aws-sdk");
+
+const TableName = "ToPlayTv-Games";
+
+if (typeof dynamoDb === "undefined") {
+  var dynamoDb = new AWS.DynamoDB.DocumentClient();
+}
+
 exports.handler = async function(event) {
-  console.log("lambda handler invoked", JSON.stringify(event));
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/json" },
-    body: JSON.stringify({
-      code: "ABCD",
-      createdAt: new Date().getTime() / 1000,
-      status: "LOBBY",
-      type: "TWOROOMSONEBOOM"
-    })
+  const gameType = event.gameType;
+  const generatedCode = Math.random()
+    .toString(36)
+    .substr(2, 4);
+
+  const Item = {
+    code: generatedCode,
+    createdAt: Math.round(new Date().getTime() / 1000),
+    status: "LOBBY",
+    type: gameType
   };
+
+  const params = {
+    TableName,
+    Item
+  };
+
+  await dynamoDb.put(params).promise();
+
+  console.log("returning", Item);
+
+  return Item;
 };
--- infrastructure/lib/api-stack.ts
+++ infrastructure/lib/api-stack.ts
@@ -1,9 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as AppSync from "@aws-cdk/aws-appsync";
 import * as Lambda from "@aws-cdk/aws-lambda";
-import { MappingTemplate } from "@aws-cdk/aws-appsync";
-
-const TABLE_NAME = "ToPlayTV-Lobby";
+import * as DynamoDb from "@aws-cdk/aws-dynamodb";
 
 // this could be a repeatable stack to make it easy to create new apps
 
@@ -11,9 +9,18 @@ export class ApiStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const gameTable = new DynamoDb.Table(this, "GameTable", {
+      tableName: "ToPlayTv-Games",
+      partitionKey: { name: "code", type: DynamoDb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
     const graphqlApi = new AppSync.GraphQLApi(this, "ToPlayTvApi", {
       name: "ToPlayTvApi",
-      schemaDefinitionFile: "../schema.graphql"
+      schemaDefinitionFile: "../schema.graphql",
+      logConfig: {
+        fieldLogLevel: AppSync.FieldLogLevel.ALL
+      }
     });
 
     const createGameLambda = new Lambda.Function(this, "CreateGameLambda", {
@@ -21,6 +28,7 @@ export class ApiStack extends cdk.Stack {
       code: Lambda.Code.asset("../functions/createGame"),
       handler: "index.handler"
     });
+    gameTable.grantReadWriteData(createGameLambda);
 
     const createGameDs = graphqlApi.addLambdaDataSource(
       "CreateGame",
@@ -30,10 +38,10 @@ export class ApiStack extends cdk.Stack {
     createGameDs.createResolver({
       typeName: "Mutation",
       fieldName: "createGame",
-      requestMappingTemplate: MappingTemplate.lambdaRequest(
+      requestMappingTemplate: AppSync.MappingTemplate.lambdaRequest(
         "$util.toJson($ctx.args.input)"
       ),
-      responseMappingTemplate: MappingTemplate.lambdaResult()
+      responseMappingTemplate: AppSync.MappingTemplate.lambdaResult()
     });
   }
 }
